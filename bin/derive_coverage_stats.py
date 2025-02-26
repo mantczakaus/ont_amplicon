8f51a48830138e5287fee0814f634d7b636e347f
maelyg <marieemilie.gauthier@qut.edu.au>
2025-02-26 11:32:09 +1000

    clean code

--- bin/derive_coverage_stats.py
+++ bin/derive_coverage_stats.py
@@ -41,8 +41,8 @@ def main():
     samtools_cov = pd.read_csv(sample_name + "_coverage.txt", sep="\t", usecols=["#rname", "endpos", "numreads", "meandepth"], header=0)
     #samtools_cov.drop(["startpos", "covbases", "coverage", "meandepth", "meanbaseq", "meanmapq"] , axis=1, inplace=True)
     samtools_cov2 = samtools_cov.copy()
-    samtools_cov2.rename(columns={"#rname": "qseqid", "endpos": "query_match_length", "numreads": "read_count", "meandepth": "mean_depth"}, inplace=True)
-    samtools_cov2['pc_read'] = samtools_cov2['read_count'] / filtered_read_counts * 100
+    samtools_cov2.rename(columns={"#rname": "qseqid", "endpos": "query_match_length", "numreads": "qseq_mapping_read_count", "meandepth": "qseq_mean_depth"}, inplace=True)
+    samtools_cov2['qseq_pc_mapping_read'] = samtools_cov2['qseq_mapping_read_count'] / filtered_read_counts * 100
     print(samtools_cov2)
     #sum_aligned_read_counts = samtools_cov2['read_count'].sum()
     #sum_unaligned= filtered_read_counts - sum_aligned_read_counts
@@ -53,23 +53,22 @@ def main():
     mosdepth.columns = ["qseqid", "start", "end", "region", "base_counts_at_depth_30X"]
     #address why the base_counts_at_depth_30X can be higher than length of the sequence, soft clipping?
     print(mosdepth)
-    mosdepth['pc_depth_30X'] = np.where(mosdepth['base_counts_at_depth_30X'] > mosdepth['end'], 100, mosdepth['base_counts_at_depth_30X'] / mosdepth['end'] * 100)
-    mosdepth['pc_depth_30X'] = mosdepth['pc_depth_30X'].apply(lambda x: float("{:.1f}".format(x)))
+    mosdepth['qseq_pc_depth_30X'] = np.where(mosdepth['base_counts_at_depth_30X'] > mosdepth['end'], 100, mosdepth['base_counts_at_depth_30X'] / mosdepth['end'] * 100)
+    mosdepth['qseq_pc_depth_30X'] = mosdepth['qseq_pc_depth_30X'].apply(lambda x: float("{:.1f}".format(x)))
     
-    mosdepth_final = mosdepth[["qseqid", "pc_depth_30X"]]
+    mosdepth_final = mosdepth[["qseqid", "qseq_pc_depth_30X"]]
 
     summary_dfs = (blast_df, samtools_cov2, mosdepth_final)
     blast_df_final = reduce(lambda left,right: pd.merge(left,right,on=["qseqid"],how='outer').fillna(0), summary_dfs)    
 
-    blast_df_final = blast_df_final.sort_values(["pc_read", "target_organism_match"], ascending = (False, False))
-    blast_df_final['status'] = np.where((blast_df_final['pc_depth_30X'] < 90), "FAIL", "PASS")
+    blast_df_final = blast_df_final.sort_values(["qseq_pc_mapping_read", "target_organism_match"], ascending = (False, False))
+    #blast_df_final['status'] = np.where((blast_df_final['pc_depth_30X'] < 90), "FAIL", "PASS")
     
     #Unmapped = {"sample_name" : sample_name, "qseqid" : "Unmapped", "consensus_seq" : "NA", "read_counts" : "NA", "sgi" : "NA", "sacc" : "NA", "length": "NA", "nident": "NA", "pident": "NA", "mismatch" : "NA", "gapopen" : "NA", "qstart" : "NA", "qend": "NA", "qlen" : "NA", "sstart": "NA", "send": "NA", "slen": "NA", "sstrand": "NA", "evalue": "NA", "bitscore": "NA", "query_coverage": "NA", "orientation": "NA", "species" : "NA", "kingdom": "NA", "full_lineage": "NA", "query_match_seq" : "NA", "query_match_length" : "NA", "read_count": sum_unaligned, "pc_read" : sum_unaligned_pc, "mean_depth" : "NA", "pc_depth_30X": "NA", "target_organism_match": "NA"}
     #blast_df_final.loc[len(blast_df_final)] = Unmapped
     #blast_df_final = blast_df_final.reset_index(drop=True)
-    blast_df_final['pc_read'] = blast_df_final['pc_read'].apply(lambda x: float("{:.1f}".format(x)))
+    blast_df_final['qseq_pc_mapping_read'] = blast_df_final['qseq_pc_mapping_read'].apply(lambda x: float("{:.1f}".format(x)))
     blast_df_final.to_csv(str(sample_name) + "_top_blast_with_cov_stats.txt", index=None, sep="\t")
-    
 
     print(blast_df_final)
 
